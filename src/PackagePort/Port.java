de87411af242eb0a2ad199dd8dfd9d8322459e16
JC <jctobosob@gmail.com>
2023-06-01 13:49:16 +0200

    C1

---
+++ src/PackagePort/Port.java
@@ -0,0 +1,37 @@
+
+package PackagePort;
+
+public class Port {
+    public Hub[] hubs = new Hub[3];
+
+    public Port(Hub firstHub, Hub secondHub, Hub thirdHub) {
+        hubs[0] = firstHub;
+        hubs[1] = secondHub;
+        hubs[2] = thirdHub;
+
+    }
+
+
+    public void checkAvailable(Container container){
+        if(hubs[0].stackContainer(container) == false){
+            if(hubs[1].stackContainer(container) == false){
+                if(hubs[2].stackContainer(container) == false){
+                    System.out.println("The level " + container.getPriority() + " is full.");
+                }
+            }
+        }
+    }
+
+    public int howManyContChecked(int priority){
+        int[] cont = new int[3];
+        int result=0;
+
+        for (int i = 0; i < 3; i++) {
+            cont[i] = hubs[i].howManyChecked(3);
+        }
+        for (int i = 0; i < 3; i++) {
+            result+=cont[i];
+        }
+        return result;
+    }
+}
